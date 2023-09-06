bb9b85eadeca753435b378075ad696019bc5aebb
Omaribrahim43 <omar.migdadi43@gmail.com>
2023-09-06 10:16:31 +0300

    omar

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello', function () {
+Route::get('/about', function () {
+    return view('welcome');
+});
+Route::get('/learn', function () {
+    return view('welcome');
+});
+Route::get('/contact', function () {
     return view('welcome');
 });
